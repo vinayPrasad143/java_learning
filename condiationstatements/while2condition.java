de052083d60ef4d18f28ab9e2a6b122938b08d49
vinayprasad.testy <vinayprasad.testy@gmail.com>
2025-01-07 21:26:46 +0530

    code push through cmd

---
+++ condiationstatements/while2condition.java
@@ -0,0 +1,15 @@
+package condiationstatements;
+
+public class while2condition {
+
+    public static void main(String[] args) {
+
+        int count =1;
+
+        while(count <= 10)
+        {
+            System.out.println("Count :" + count);
+            count++;
+        }
+    }
+}
---
+++ condiationstatements/whilecondition.java
@@ -0,0 +1,4 @@
+package condiationstatements;
+
+public class whilecondition {
+}
