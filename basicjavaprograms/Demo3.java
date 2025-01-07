0fd97e65063484a6c29e26b733869f07042c5a7b
vinayprasad.testy <vinayprasad.testy@gmail.com>
2025-01-07 18:10:57 +0530

    craeted 4 demos
    second push to cloud

---
+++ basicjavaprograms/Demo3.java
@@ -0,0 +1,21 @@
+package basicjavaprograms;
+
+public class Demo3 {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<=5; i++)
+        {
+            for (int j=0; j<=5; j++)
+            {
+                if(i<=j)
+                {
+                    System.out.print(i);
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
---
+++ basicjavaprograms/demo4.java
@@ -0,0 +1,17 @@
+package basicjavaprograms;
+
+public class demo4 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                if (i >= j) {
+                    System.out.print(i);
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
