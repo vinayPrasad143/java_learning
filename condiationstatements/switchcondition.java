3d1eb5d8785c91b5e8af054fb8be4ba0efdebaf7
vinayprasad.testy <vinayprasad.testy@gmail.com>
2025-01-07 21:40:51 +0530

    switch code

---
+++ condiationstatements/switchcondition.java
@@ -0,0 +1,32 @@
+package condiationstatements;
+
+public class switchcondition {
+
+    public static void main(String[] args) {
+
+        int i= 100;
+
+        switch (i)
+        {
+            case 2:
+                System.out.println(i);
+                break;
+
+            case 4:
+                System.out.println(i);
+                break;
+
+            case 6:
+                System.out.println(i);
+                break;
+
+            case 10:
+                System.out.println(i);
+                break;
+
+            default:
+                System.out.println("Nothing");
+
+        }
+    }
+}
